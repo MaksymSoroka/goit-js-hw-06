eeb5698ae6c03b5636d1e719e6c3cf5ad0b50bed
Maksym Soroka <masek4ever@gmail.com>
2024-07-14 19:48:29 +0200

    gotovella

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,25 @@
-function slugify(title) {
-  const lowSlug = title.toLowerCase();
-  const slug = lowSlug.split(` `);
-  return slug.join(`-`)
+function isEnoughCapacity(products, containerSize) {
+  const product = Object.values(products);
+  let sum = 0;
+  for (let i of product) {
+    sum += i;
+  }
+  return sum <= containerSize;
+  
 }
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
